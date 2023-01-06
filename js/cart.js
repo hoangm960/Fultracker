ba006b241058aa2d3bdb39ad773142e359c5ff9b
hoangm960 <63330514+hoangm960@users.noreply.github.com>
2023-01-07 02:58:46 +0700

    fix category

--- js/cart.js
+++ js/cart.js
@@ -51,6 +51,7 @@ function removeFromCart(event) {
     var button = event.currentTarget;
     var course = button.parentElement.parentElement
     var title = course.getElementsByClassName("course-name")[0].innerText
+    document.cookie = `title=${title};`
     addToHome(title)
     course.remove()
     getCourse(-1)
@@ -86,14 +87,13 @@ function getCourse(mode) {
         .then(response => response.json())
         .then(json => {
             course = json.find(element => element['name'] == document.cookie.split(';')[0].split('=')[1])
-            updateFooter(course['major'], 12 * mode, [course['category'], 1 * mode])
+            // updateProgressBar(course['major'], 12 * mode)
+            updateCategory(course['category'], mode)
         })
 
 }
 
-function updateFooter(major, percent, category_dict) {
-    // Update major progress bar
-
+function updateProgressBar(major, percent) {
     localStorage[major] = String(parseInt(localStorage[major]) + percent)
     footer = document.getElementsByClassName('footer')[0]
     var [major, minor] = footer.getElementsByClassName('progress-container')
@@ -110,21 +110,22 @@ function updateFooter(major, percent, category_dict) {
         progress_minor = document.getElementById(`progress-2`)
         progress_minor.style.width = `${localStorage[minor_val]}%`
     }
+}
 
-    // Update category
+function updateCategory(category_dict, mode) {
     category_section = document.getElementsByClassName("category-section")[0]
-    categories = category_section.getElementsByTagName("p")
+    categoryElements = category_section.getElementsByTagName("p")
     category_indexes = []
-    for (var i = 0; i < categories.length; i++) {
-        for (var j = 0; j < category_dict[0].length; j++) {
-            if (categories[i].innerText.split(": ")[0] == category_dict[0][j].split(" (")[0]) {
+    for (var i = 0; i < categoryElements.length; i++) {
+        for (var j = 0; j < category_dict.length; j++) {
+            if (categoryElements[i].innerText.split(": ")[0] == category_dict[j].split(" (")[0]) {
                 category_indexes.push(i)
             }
         }
     }
+
+    counters = category_section.getElementsByClassName("counter")
     category_indexes.forEach(i => {
-        category_texts = categories[i].innerText.split(": ")
-        cat_val = parseInt(category_texts[1].slice(0, -2)) + category_dict[1]
-        categories[i].innerHTML = `${category_texts[0]}: ${cat_val}/2`
+        counters[i].innerHTML = parseInt(counters[i].innerText) + mode
     });
 }
