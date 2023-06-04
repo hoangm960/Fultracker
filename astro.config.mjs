7dd2c1d8d5158c49d822b18b09eec78295e59803
hoangm960 <hoangm960@gmail.com>
2023-06-04 18:34:55 +0700

    fix not deploy

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,7 +1,11 @@
 import { defineConfig } from 'astro/config';
+import node from '@astrojs/node';
 
 // https://astro.build/config
 export default defineConfig({
     output: 'server',
     site: 'https://hoangm960.github.io/Fultracker',
+    adapter: node({
+        mode: 'standalone'
+    }),
 });
