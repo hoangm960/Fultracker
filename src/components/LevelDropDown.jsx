0308f5b218fd4af9bdeb5778718c2d3641afeae2
hoangm960 <hoangm960@gmail.com>
2023-06-17 01:20:11 +0700

    update UI

--- src/components/LevelDropDown.jsx
+++ src/components/LevelDropDown.jsx
@@ -7,7 +7,7 @@ export default class LevelDropDown extends Component {
         <div class="absolute h-fit -top-4 left-4 px-2 bg-dark-brown text-lg font-light italic text-white text-center ">
             Academic Level 
         </div>
-        <select class="w-full h-4/5 pl-2 bg-transparent border-4 border-light-primary rounded-2xl box-border cursor-pointer" name="level" id="level">
+        <select class="w-full pl-3 pt-2 pb-2 bg-transparent border-4 border-light-primary rounded-2xl box-border cursor-pointer" name="level" id="level">
             <option value="freshman">Freshman </option>
             <option value="sophomore">Sophomore</option>
             <option value="junior">Junior</option>
