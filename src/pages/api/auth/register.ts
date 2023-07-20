06ba80392b4856c283860966580dc7c40c1b16f8
hoangm960 <63330514+hoangm960@users.noreply.github.com>
2023-07-20 11:01:41 +0700

    handle email already exist

--- src/pages/api/auth/register.ts
+++ src/pages/api/auth/register.ts
@@ -3,31 +3,36 @@ import { getAuth } from "firebase-admin/auth";
 import { app } from "@lib/firebase/server";
 
 export const post: APIRoute = async ({ request, redirect }) => {
-  const auth = getAuth(app);
+	const auth = getAuth(app);
 
-  /* Get form data */
-  const formData = await request.formData();
-  const email = formData.get("email")?.toString();
-  const password = formData.get("password")?.toString();
+	/* Get form data */
+	const formData = await request.formData();
+	const email = formData.get("email")?.toString();
+	const password = formData.get("password")?.toString();
 
-  if (!email || !password) {
-    return new Response(
-      "Missing form data",
-      { status: 400 }
-    );
-  }
+	if (!email || !password) {
+		return new Response(
+			"Missing form data",
+			{ status: 400 }
+		);
+	}
 
-  /* Create user */
-  try {
-    await auth.createUser({
-      email,
-      password,
-    });
-  } catch (error: any) {
-    return new Response(
-      `Something went wrong: ${error}`,
-      { status: 400 }
-    );
-  }
-  return redirect("/signin");
+	/* Create user */
+	try {
+		await auth.createUser({
+			email,
+			password,
+		});
+	} catch (error: any) {
+		if (error.code == "auth/email-already-exists") {
+			// TODO: add alert for user
+			return redirect("/signin");
+		};
+
+		return new Response(
+			`Something went wrong: ${error}`,
+			{ status: 400 }
+		);
+	}
+	return redirect("/signin");
 };
