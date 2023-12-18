9c1eb9d359b9f19fe67b0f7589cdd309fdc1c458
hoangm960 <63330514+hoangm960@users.noreply.github.com>
2023-12-18 23:59:42 +0700

    add flowchart component

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,13 +1,14 @@
 import { defineConfig } from 'astro/config';
-import netlify from "@astrojs/netlify/functions";
-import react from "@astrojs/react";
 
+// import netlify from "@astrojs/netlify/functions";
+import react from "@astrojs/react";
 import tailwind from "@astrojs/tailwind";
+
 
 // https://astro.build/config
 export default defineConfig({
   output: 'server',
-  adapter: netlify(),
+  // adapter: netlify(),
   integrations: [react(), tailwind()],
-  prefetch: true
+  prefetch: true,
 });
