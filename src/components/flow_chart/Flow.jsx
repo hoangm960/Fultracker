4e3a78e037df268e178ea41e8181d90a243bef10
hoangm960 <63330514+hoangm960@users.noreply.github.com>
2023-12-21 01:40:28 +0700

    connect main nodes

--- src/components/flow_chart/Flow.jsx
+++ src/components/flow_chart/Flow.jsx
@@ -2,46 +2,72 @@ import ReactFlow from 'reactflow';
 import MainBlock from './MainBlock';
 import 'reactflow/dist/style.css';
 
-import flowData from "@data/flow_chart.json"
+import majorData from "@data/major.json"
 
-const rfStyle = {
-  backgroundColor: '#dffcf6',
-};
+function getNodesAndEdges(flowData) {
+  let nodes = [
+    {
+      id: flowData[0]['id'],
+      type: 'mainBlock',
+      position: { x: 0, y: 0 },
+      data: flowData[0]
+    }
+  ];
+  
+  let edges = [];
+  
+  for (let i = 1; i < flowData.length; i++) {
+    const previousElement = flowData[i - 1];
+    const currentElement = flowData[i];
+    let nodeData = {
+      id: currentElement['id'],
+      type: 'mainBlock',
+      targetPosition: 'bottom',
+      position: { x: 0, y: -(80 * i) },
+      data: currentElement
+    };
+    let edgeData = {
+      id: `edge-${i}`,
+      source: previousElement['id'],
+      target: currentElement['id'],
+      sourceHandle: "main"
+    };
+    if (Array.isArray(currentElement)) {
+      for (let j = 0; j < currentElement.length; j++) {
+        const element = currentElement[j];
+        nodeData = {
+          id: element['id'],
+          type: 'mainBlock',
+          targetPosition: 'bottom',
+          position: { x: 200*(j + 1 - currentElement.length / 2), y: -(80 * i) },
+          data: element
+        };
+        edgeData = {
+          id: `edge-${i}-${j+1}`,
+          source: previousElement['id'],
+          target: element['id'],
+          sourceHandle: "main"
+        };
+        
+        nodes.push(nodeData);
+        edges.push(edgeData);
+      };
+    } else{ 
+      nodes.push(nodeData);
+      edges.push(edgeData);
+    }
+  }
+  
+  return [nodes, edges];
+}
+
 const proOptions = { hideAttribution: true };
 
-const flows = flowData["MATH"]["requirements"]["major"]["flow"]
+const flowData = majorData["MATH"]["requirements"]["major"]["flow"];
+console.log(getNodesAndEdges(flowData));
+let [nodes, edges] = getNodesAndEdges(flowData);
 
-const nodes = [
-  {
-    id: flows[0]["id"],
-    type: 'MainBlock',
-    position: { x: 0, y: 0 },
-    data: flows[0]
-  },
-  {
-    id: 'node-2',
-    type: 'output',
-    targetPosition: 'top',
-    position: { x: 0, y: 200 },
-    data: { label: 'node 2' },
-  },
-  {
-    id: 'node-3',
-    type: 'output',
-    targetPosition: 'top',
-    position: { x: 200, y: 200 },
-    data: { label: 'node 3' },
-  },
-];
-
-const edges = [
-  { id: 'edge-1', source: flows[0]["id"], target: 'node-2'},
-  { id: 'edge-2', source: flows[0]["id"], target: 'node-3'},
-];
-
-// we define the nodeTypes outside of the component to prevent re-renderings
-// you could also use useMemo inside the component
-const nodeTypes = { MainBlock: MainBlock };
+const nodeTypes = { mainBlock: MainBlock };
 
 function Flow() {
   return (
@@ -50,7 +76,6 @@ function Flow() {
       edges={edges}
       nodeTypes={nodeTypes}
       fitView
-      style={rfStyle}
       proOptions={proOptions}
     />
   );
--- src/components/flow_chart/MainBlock.jsx
+++ src/components/flow_chart/MainBlock.jsx
@@ -1,21 +1,36 @@
 import { Handle, Position } from 'reactflow';
 
-function TextUpdaterNode({ data }) {
-  console.log(data);
+function MainBlock({ data }) {
   return (
-    <div className="h-12 border-2 border-solid p-1 bg-white">
-      <Handle
-        type="target"
-        position={Position.Top}
-        isConnectable={false}
-      />
-
+    <div className="h-12 border-[1px] border-black border-solid rounded-md p-1 bg-white">
       <p>
         {data["name"]}
       </p>
 
       <Handle
+        id="main"
         type="source"
+        position={Position.Top}
+        isConnectable={false}
+        />
+
+      <Handle
+        id="course"
+        type="source"
+        position={Position.Right}
+        isConnectable={false}
+      />
+
+      <Handle
+        id="course"
+        type="source"
+        position={Position.Left}
+        isConnectable={false}
+      />
+
+      <Handle
+        id="course"
+        type="target"
         position={Position.Bottom}
         isConnectable={false}
       />
@@ -23,4 +38,4 @@ function TextUpdaterNode({ data }) {
   );
 }
 
-export default TextUpdaterNode;
+export default MainBlock;
