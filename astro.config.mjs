41be59d4b171844cfe01ab5020cff4887bc1b7ac
hoangm960 <63330514+hoangm960@users.noreply.github.com>
2023-12-22 04:06:01 +0700

    fix server build

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,14 +1,13 @@
 import { defineConfig } from 'astro/config';
 
-// import netlify from "@astrojs/netlify/functions";
+import netlify from "@astrojs/netlify/functions";
 import react from "@astrojs/react";
 import tailwind from "@astrojs/tailwind";
 
 
-// https://astro.build/config
 export default defineConfig({
   output: 'server',
-  // adapter: netlify(),
+  adapter: netlify(),
   integrations: [react(), tailwind()],
   prefetch: true,
 });
