ad1ca8f3f045004c2cca3db08ed75c8fc6e19734
hoangm960 <63330514+hoangm960@users.noreply.github.com>
2023-12-26 01:43:53 +0700

    add declair to flow

--- src/components/flow_chart/FlowProvider.jsx
+++ src/components/flow_chart/FlowProvider.jsx
@@ -11,111 +11,47 @@ import ReactFlow, {
 import MainBlock from "./MainBlock";
 import CourseBlock from "./CourseBlock";
 import "reactflow/dist/style.css";
-
-function getNodesAndEdges(flowData) {
-  let nodes = [];
-  for (const [index, [id, node]] of Object.entries(
-    Object.entries(flowData["nodes"])
-  )) {
-    nodes.push({
-      id: id,
-      type: "mainBlock",
-      targetPosition: "bottom",
-      position: { x: 0, y: -(80 * index) },
-      data: node,
-    });
-  }
-
-  let edges = [];
-  for (const [i, [id, flow]] of Object.entries(
-    Object.entries(flowData["flows"]["main"])
-  )) {
-    for (const [j, nextNodeID] of Object.entries(flow)) {
-      edges.push({
-        id: `${id}-${nextNodeID}`,
-        source: id,
-        target: nextNodeID,
-        sourceHandle: "main",
-      });
-      console.log(
-        id,
-        nodes.find((node) => node.id === id),
-        i,
-        j
-      );
-      nodes.find((node) => node.id === nextNodeID)["position"]["x"] =
-        nodes.find((node) => node.id === id)["position"]["x"] +
-        300 * (+j + 0.5 - flow.length / 2);
-      nodes.find((node) => node.id === nextNodeID)["position"]["y"] =
-        nodes.find((node) => node.id === id)["position"]["y"] - 100;
-    }
-  }
-
-  if (flowData["flows"]["course"]) {
-    for (const [nodeID, node] of Object.entries(flowData["flows"]["course"])) {
-      const courses = node["courses"];
-      const nodeIdx = nodes.findIndex((node) => node["id"] == nodeID);
-      for (let courseIdx = 0; courseIdx < courses.length; courseIdx++) {
-        const courseID = courses[courseIdx];
-        const nodeData = {
-          id: courseID,
-          type: "courseBlock",
-          position: {
-            x: nodeIdx % 2 === 0 ? 300 : -150,
-            y:
-              60 * (courseIdx + 0.5 - courses.length / 2) +
-              nodes[nodeIdx]["position"]["y"],
-          },
-          data: {
-            course: courseID,
-            position: nodeIdx % 2 === 0 ? "left" : "right",
-          },
-        };
-        const edgeData = {
-          id: `${nodeID}-${courseID}`,
-          source: nodeID,
-          target: courseID,
-          sourceHandle: nodeIdx % 2 === 0 ? "courseRight" : "courseLeft",
-        };
-
-        nodes.push(nodeData);
-        edges.push(edgeData);
-      }
-    }
-  }
-
-  console.log(nodes, edges);
-  return [nodes, edges];
-}
+import DeclairCheckBox from "./DeclairCheckBox";
 
 const proOptions = { hideAttribution: true };
 const nodeTypes = { mainBlock: MainBlock, courseBlock: CourseBlock };
 
-const ProviderFlow = () => {
+export default function ProviderFlow() {
   const [nodes, setNodes, onNodesChange] = useNodesState([]);
   const [edges, setEdges, onEdgesChange] = useEdgesState([]);
 
   const updateFlow = () => {
     const majorSelect = document.getElementById("major");
     const flowRadios = document.getElementsByName("flow-radio");
+    const declairCheckBox = document.getElementById("declair");
+
     let selectedFlow;
     flowRadios.forEach((radio) => {
       if (radio.checked) {
         selectedFlow = radio.value;
       }
     });
-    const [newNodes, newEdges] = getNodesAndEdges(
-      majorData[majorSelect.value.toUpperCase()]["flow-chart"][selectedFlow]
-    );
-    setNodes(newNodes);
-    setEdges(newEdges);
+    const majorValue = majorSelect.value.toUpperCase();
+    const chartOption = declairCheckBox.checked ? "flow-chart" : "declair";
+
+    if (majorValue && selectedFlow) {
+      const [newNodes, newEdges] = getNodesAndEdges(
+        majorData[majorValue][chartOption][selectedFlow]
+      );
+      setNodes(newNodes);
+      setEdges(newEdges);
+    }
   };
 
   return (
     <ReactFlowProvider>
-      <div className="flex flex-row w-full gap-1">
+      <div className="flex flex-row w-full gap-1 justify-center">
         <MajorDropdown updateFlow={updateFlow} />
-        <FlowRadioBox updateFlow={updateFlow} />
+        <div id="radio-box" className="flex flex-col items-center justify-center gap-1" onChange={updateFlow}>
+          <FlowRadioBox value="major" label="Major" checked />
+          <FlowRadioBox value="minor" label="Minor" />
+        </div>
+        <DeclairCheckBox updateFlow={updateFlow} />
       </div>
       <div id="flow-box" className="h-full w-full">
         <ReactFlow
@@ -133,6 +69,76 @@ const ProviderFlow = () => {
       </div>
     </ReactFlowProvider>
   );
-};
+}
 
-export default ProviderFlow;
+function getNodesAndEdges(flowData) {
+  let nodes = [];
+  for (const [index, [id, node]] of Object.entries(
+    Object.entries(flowData["nodes"])
+  )) {
+    nodes.push({
+      id: id,
+      type: "mainBlock",
+      targetPosition: "bottom",
+      position: { x: 0, y: -(80 * index) },
+      data: node,
+    });
+  }
+
+  let edges = [];
+  if (flowData["flows"]) {
+    for (const [i, [id, flow]] of Object.entries(
+      Object.entries(flowData["flows"]["main"])
+    )) {
+      for (const [j, nextNodeID] of Object.entries(flow)) {
+        edges.push({
+          id: `${id}-${nextNodeID}`,
+          source: id,
+          target: nextNodeID,
+          sourceHandle: "main",
+        });
+
+        nodes.find((node) => node.id === nextNodeID)["position"]["x"] =
+          nodes.find((node) => node.id === id)["position"]["x"] +
+          300 * (+j + 0.5 - flow.length / 2);
+        nodes.find((node) => node.id === nextNodeID)["position"]["y"] =
+          nodes.find((node) => node.id === id)["position"]["y"] - 100;
+      }
+    }
+
+    if (flowData["flows"]["course"]) {
+      for (const [nodeID, node] of Object.entries(flowData["flows"]["course"])) {
+        const courses = node["courses"];
+        const nodeIdx = nodes.findIndex((node) => node["id"] == nodeID);
+        for (let courseIdx = 0; courseIdx < courses.length; courseIdx++) {
+          const courseID = courses[courseIdx];
+          const nodeData = {
+            id: courseID,
+            type: "courseBlock",
+            position: {
+              x: nodeIdx % 2 === 0 ? 300 : -150,
+              y:
+                60 * (courseIdx + 0.5 - courses.length / 2) +
+                nodes[nodeIdx]["position"]["y"],
+            },
+            data: {
+              course: courseID,
+              position: nodeIdx % 2 === 0 ? "left" : "right",
+            },
+          };
+          const edgeData = {
+            id: `${nodeID}-${courseID}`,
+            source: nodeID,
+            target: courseID,
+            sourceHandle: nodeIdx % 2 === 0 ? "courseRight" : "courseLeft",
+          };
+
+          nodes.push(nodeData);
+          edges.push(edgeData);
+        }
+      }
+    }
+  }
+
+  return [nodes, edges];
+}
--- src/components/flow_chart/FlowRadioBox.jsx
+++ src/components/flow_chart/FlowRadioBox.jsx
@@ -1,39 +1,23 @@
-function FlowRadioBox({updateFlow}) {
+export default function FlowRadioBox(props) {
     return (
-        <div id="radio-box" className="flex flex-col items-center justify-center gap-1" onChange={updateFlow}>
-            <div
-                className="flex items-center px-4 h-10 bg-action border-2 border-text rounded"
+        <div
+            className="flex items-center px-4 h-10 bg-action border-2 border-text rounded"
+        >
+            <input
+                defaultChecked={props.checked}
+                id="major"
+                type="radio"
+                value={props.value}
+                name="flow-radio"
+                className="w-4 h-4 text-blue-600 bg-gray-100 border-gray-300"
+            />
+            <label
+                htmlFor="major"
+                className="w-full py-4 ms-2 text-sm font-semibold text-highlight"
             >
-                <input
-                    defaultChecked
-                    id="major"
-                    type="radio"
-                    value="major"
-                    name="flow-radio"
-                    className="w-4 h-4 text-blue-600 bg-gray-100 border-gray-300 focus:ring-blue-500 dark:focus:ring-blue-600 focus:ring-2"
-                />
-                <label
-                    htmlFor="major"
-                    className="w-full py-4 ms-2 text-sm font-semibold text-highlight">Major</label
-                >
-            </div>
-            <div
-                className="flex items-center px-4 h-10 bg-action border-2 border-text rounded"
-            >
-                <input
-                    id="minor"
-                    type="radio"
-                    value="minor"
-                    name="flow-radio"
-                    className="w-4 h-4 text-blue-600 bg-gray-100 border-gray-300 focus:ring-blue-500 dark:focus:ring-blue-600 focus:ring-2"
-                />
-                <label
-                    htmlFor="minor"
-                    className="w-full py-4 ms-2 text-sm font-semibold text-highlight">Minor</label
-                >
-            </div>
+                {props.label}
+            </label>
         </div>
     );
 }
 
-export default FlowRadioBox;
