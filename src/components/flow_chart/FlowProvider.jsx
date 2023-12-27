a6ca8d76380db97ef463be7bc8549b56b4cdf0c9
hoangm960 <63330514+hoangm960@users.noreply.github.com>
2023-12-27 12:11:02 +0700

    add subflow

--- src/components/flow_chart/FlowProvider.jsx
+++ src/components/flow_chart/FlowProvider.jsx
@@ -86,10 +86,8 @@ function getNodesAndEdges(flowData) {
   }
 
   let edges = [];
-  if (flowData["flows"]) {
-    for (const [i, [id, flow]] of Object.entries(
-      Object.entries(flowData["flows"]["main"])
-    )) {
+  if (flowData["flows"]["main"]) {
+    for (const [id, flow] of Object.entries(flowData["flows"]["main"])) {
       for (const [j, nextNodeID] of Object.entries(flow)) {
         edges.push({
           id: `${id}-${nextNodeID}`,
@@ -105,39 +103,60 @@ function getNodesAndEdges(flowData) {
           nodes.find((node) => node.id === id)["position"]["y"] - 100;
       }
     }
+  }
 
-    if (flowData["flows"]["course"]) {
-      for (const [nodeID, node] of Object.entries(flowData["flows"]["course"])) {
-        const courses = node["courses"];
-        const nodeIdx = nodes.findIndex((node) => node["id"] == nodeID);
-        for (let courseIdx = 0; courseIdx < courses.length; courseIdx++) {
-          const courseID = courses[courseIdx];
-          const nodeData = {
-            id: courseID,
-            type: "courseBlock",
-            position: {
-              x: nodeIdx % 2 === 0 ? 300 : -150,
-              y:
-                60 * (courseIdx + 0.5 - courses.length / 2) +
-                nodes[nodeIdx]["position"]["y"],
-            },
-            data: {
-              course: courseID,
-              position: nodeIdx % 2 === 0 ? "left" : "right",
-            },
-          };
-          const edgeData = {
-            id: `${nodeID}-${courseID}`,
-            source: nodeID,
-            target: courseID,
-            sourceHandle: nodeIdx % 2 === 0 ? "courseRight" : "courseLeft",
-          };
+  if (flowData["flows"]["sub-flows"]) {
+    for (const [id, flow] of Object.entries(flowData["flows"]["sub-flows"])) {
+      for (const [j, nextNodeID] of Object.entries(flow)) {
+        edges.push({
+          id: `${id}-${nextNodeID}`,
+          source: id,
+          target: nextNodeID,
+          sourceHandle: "courseRight",
+          targetHandle: "courseLeft",
+        });
 
-          nodes.push(nodeData);
-          edges.push(edgeData);
-        }
+        nodes.find((node) => node.id === nextNodeID)["position"]["x"] =
+          nodes.find((node) => node.id === id)["position"]["x"] + 300;
+        nodes.find((node) => node.id === nextNodeID)["position"]["y"] =
+          10 * (j + 0.5 - flow.length / 2) +
+          nodes.find((node) => node.id === id)["position"]["y"];
       }
     }
+  }
+
+  if (flowData["flows"]["course"]) {
+    for (const [nodeID, node] of Object.entries(flowData["flows"]["course"])) {
+      const courses = node["courses"];
+      const nodeIdx = nodes.findIndex((node) => node["id"] == nodeID);
+      for (let courseIdx = 0; courseIdx < courses.length; courseIdx++) {
+        const courseID = courses[courseIdx];
+        const nodeData = {
+          id: courseID,
+          type: "courseBlock",
+          position: {
+            x: nodeIdx % 2 === 0 ? 300 : -150,
+            y:
+              60 * (courseIdx + 0.5 - courses.length / 2) +
+              nodes[nodeIdx]["position"]["y"],
+          },
+          data: {
+            course: courseID,
+            position: nodeIdx % 2 === 0 ? "left" : "right",
+          },
+        };
+        const edgeData = {
+          id: `${nodeID}-${courseID}`,
+          source: nodeID,
+          target: courseID,
+          sourceHandle: nodeIdx % 2 === 0 ? "courseRight" : "courseLeft",
+        };
+
+        nodes.push(nodeData);
+        edges.push(edgeData);
+      }
+    }
+
   }
 
   return [nodes, edges];
