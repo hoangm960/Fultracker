a552df1d4409eefda099d208b58b5fb7a087544f
hoangm960 <63330514+hoangm960@users.noreply.github.com>
2024-01-01 04:07:36 +0700

    update dropdown list

--- src/components/flow_chart/MajorDropdown.jsx
+++ src/components/flow_chart/MajorDropdown.jsx
@@ -10,38 +10,38 @@ function MajorDropdown({ updateFlow }) {
         {" "}
         Choose a major ...
       </option>
-      <option className="bg-background text-text font-semibold" value="cs">
-        Computer Science
-      </option>
       <option className="bg-background text-text font-semibold" value="math">
         Mathematics
-      </option>
-      <option className="bg-background text-text font-semibold" value="psy">
-        Psychology
-      </option>
-      <option className="bg-background text-text font-semibold" value="vs">
-        Vietnam Studies
-      </option>
-      <option className="bg-background text-text font-semibold" value="eng">
-        Engineering
-      </option>
-      <option className="bg-background text-text font-semibold" value="is">
-        Itegrated Science
       </option>
       <option className="bg-background text-text font-semibold" value="arts">
         Arts
       </option>
+      <option className="bg-background text-text font-semibold" value="cs">
+        Computer Science
+      </option>
       <option className="bg-background text-text font-semibold" value="econ">
         Economics
+      </option>
+      <option className="bg-background text-text font-semibold" value="eng">
+        Engineering
       </option>
       <option className="bg-background text-text font-semibold" value="his">
         History
       </option>
+      <option className="bg-background text-text font-semibold" value="is">
+        Itegrated Science
+      </option>
       <option className="bg-background text-text font-semibold" value="lit">
         Literature
       </option>
+      <option className="bg-background text-text font-semibold" value="psy">
+        Psychology
+      </option>
       <option className="bg-background text-text font-semibold" value="ss">
         Social Science
+      </option>
+      <option className="bg-background text-text font-semibold" value="vs">
+        Vietnam Studies
       </option>
     </select>
   );
