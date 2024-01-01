02b8709148da026b7195a078f3b5d6f99f26e3fb
hoangm960 <63330514+hoangm960@users.noreply.github.com>
2024-01-02 06:37:41 +0700

    fix no flow or chart option

--- src/components/flow_chart/FlowProvider.jsx
+++ src/components/flow_chart/FlowProvider.jsx
@@ -15,6 +15,7 @@ import "reactflow/dist/style.css";
 import DeclairCheckBox from "./DeclairCheckBox";
 import getNodesAndEdges from "@scripts/getFlowFromMajor";
 import FloatingEdge from "@components/flow_chart/FloatingEdge";
+import { useState } from "react";
 
 
 const proOptions = { hideAttribution: true };
@@ -24,11 +25,34 @@ const edgeTypes = { floating: FloatingEdge };
 export default function ProviderFlow() {
   const [nodes, setNodes, onNodesChange] = useNodesState([]);
   const [edges, setEdges, onEdgesChange] = useEdgesState([]);
+  const [showFlowSelection, setShowFlowSelection] = useState(false);
+  const [showChartSelection, setShowChartSelection] = useState(false);
 
-  const updateFlow = () => {
+  const updateFlow = (flow, chart) => {
     const majorSelect = document.getElementById("major");
     const flowRadios = document.getElementsByName("flow-radio");
     const declairCheckBox = document.getElementById("declair");
+
+    const majorValue = majorSelect.value.toUpperCase();
+    if ((chart !== undefined) & (flow !== undefined)) {
+      const [newNodes, newEdges] = getNodesAndEdges(
+        majorData[majorValue][chart][flow]
+      );
+      setNodes(newNodes);
+      setEdges(newEdges);
+
+      setShowChartSelection(Object.keys(majorData[majorValue]).includes("declair"));
+      setShowFlowSelection(Object.keys(majorData[majorValue][chart]).includes("minor"));
+      return;
+    }
+
+    const chartOption = 
+      declairCheckBox ? 
+        declairCheckBox.checked ? 
+          "flow-chart" 
+          : "declair" 
+        : "flow-chart";
+    setShowFlowSelection(Object.keys(majorData[majorValue][chartOption]).includes("minor"));
 
     let selectedFlow;
     flowRadios.forEach((radio) => {
@@ -36,27 +60,28 @@ export default function ProviderFlow() {
         selectedFlow = radio.value;
       }
     });
-    const majorValue = majorSelect.value.toUpperCase();
-    const chartOption = declairCheckBox.checked ? "flow-chart" : "declair";
 
-    if (majorValue && selectedFlow) {
-      const [newNodes, newEdges] = getNodesAndEdges(
-        majorData[majorValue][chartOption][selectedFlow]
-      );
-      setNodes(newNodes);
-      setEdges(newEdges);
-    }
+    const [newNodes, newEdges] = getNodesAndEdges(
+      majorData[majorValue][chartOption][selectedFlow]
+    );
+    setNodes(newNodes);
+    setEdges(newEdges);
+
   };
 
   return (
     <ReactFlowProvider>
       <div className="flex flex-row w-full gap-1 justify-center">
-        <MajorDropdown updateFlow={updateFlow} />
-        <div id="radio-box" className="flex flex-col items-center justify-center gap-1" onChange={updateFlow}>
-          <FlowRadioBox value="major" label="Major" checked />
-          <FlowRadioBox value="minor" label="Minor" />
-        </div>
-        <DeclairCheckBox updateFlow={updateFlow} />
+        <MajorDropdown updateFlow={() => updateFlow("major", "flow-chart")} />
+        {showChartSelection ?
+          <DeclairCheckBox updateFlow={updateFlow} />
+          : null}
+        {showFlowSelection ?
+          <div id="radio-box" className="flex flex-col items-center justify-center gap-1" onChange={updateFlow}>
+            <FlowRadioBox id="major-radio" value="major" label="Major" checked />
+            <FlowRadioBox id="minor-radio" value="minor" label="Minor" />
+          </div>
+          : null}
       </div>
       <div id="flow-box" className="h-full w-full">
         <ReactFlow
