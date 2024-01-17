4d72fcb12afd0b76865eb616d2e903f65ad508ad
hoangm960 <63330514+hoangm960@users.noreply.github.com>
2024-01-17 18:47:09 +0700

    add strike out done course in flowchart

--- src/components/flow_chart/CourseBlock.jsx
+++ src/components/flow_chart/CourseBlock.jsx
@@ -1,11 +1,19 @@
 import { memo } from 'react';
 import { Handle, Position } from 'reactflow';
 
+
 export default memo(({ data }) => {
+  const title = data.disabled ? 
+  <del>
+    {data["courseID"]}
+  </del> : 
+  data["courseID"];
+  
   return (
-    <div className="h-12 w-24 border-[1px] border-black border-solid rounded-md p-1 bg-highlight text-text text-center flex items-center justify-center hover:shadow-2xl hover:opacity-80">
+    <div className="h-12 w-24 border-[1px] border-text border-solid rounded-md p-1 bg-highlight text-text text-center flex items-center justify-center hover:shadow-2xl hover:opacity-80">
       <p className="text-lg font-semibold">
-        {data["courseID"]}
+
+        {title}
       </p>
 
       <Handle
--- src/components/flow_chart/FlowProvider.jsx
+++ src/components/flow_chart/FlowProvider.jsx
@@ -1,6 +1,12 @@
 import majorData from "@data/major.json";
+
 import MajorDropdown from "@components/flow_chart/MajorDropdown";
 import FlowRadioBox from "@components/flow_chart/FlowRadioBox";
+import DeclairCheckBox from "@components/flow_chart/DeclairCheckBox";
+import HomeButton from "@components/flow_chart/HomeButton.tsx";
+import MainBlock from "@components/flow_chart/MainBlock";
+import CourseBlock from "@components/flow_chart/CourseBlock";
+
 import ReactFlow, {
   Background,
   Controls,
@@ -10,14 +16,11 @@ import ReactFlow, {
   useEdgesState,
   useReactFlow
 } from "reactflow";
-import MainBlock from "./MainBlock";
-import CourseBlock from "./CourseBlock";
 import "reactflow/dist/style.css";
-import DeclairCheckBox from "./DeclairCheckBox";
-import getNodesAndEdges from "@scripts/getFlowFromMajor";
 import { useEffect, useState } from "react";
+
 import ELK from 'elkjs/lib/elk.bundled.js';
-import HomeButton from "./HomeButton.tsx";
+import getNodesAndEdges from "@scripts/getFlowFromMajor";
 
 
 const proOptions = { hideAttribution: true };
@@ -248,15 +251,31 @@ function Flow() {
       });
     }
     fitView({ duration: 800 });
+    setTimeout(() => {
+      if (nodes != undefined) {
+        fitView({ duration: 800, nodes: nodes.slice(0, 3) });
+      }
+    }, 1000);
   }, [nodes, edges]);
 
-  const onNodeClick = (event, node) => {
-    const [newNodes, newEdges] = getNodesAndEdges(node.data.children, node, true);
+  const onNodeClick = (event, nodeClicked) => {
+    const [newNodes, newEdges] = getNodesAndEdges(nodeClicked.data.children, nodeClicked, true);
     getLayoutedElements(newNodes, newEdges, "RIGHT").then(({ nodes: layoutedNodes, edges: layoutedEdges }) => {
-      fitView({ duration: 800, nodes: [node] });
+      fitView({ duration: 800, nodes: [nodeClicked] });
       setTimeout(() => {
         setPrevNodes(layoutedNodes);
-        setNodes(layoutedNodes);
+
+        const deltaPosition = {
+          x: nodeClicked.x - layoutedNodes[0].x,
+          y: nodeClicked.y - layoutedNodes[0].y
+        };
+        setNodes(layoutedNodes.map((node) => ({
+          ...node,
+          position: {
+            x: node.x + deltaPosition.x,
+            y: node.y + deltaPosition.y
+          }
+        })));
         setEdges(layoutedEdges);
       }, 1000);
     });
@@ -275,6 +294,7 @@ function Flow() {
         connectionMode={ConnectionMode.Loose}
         onNodeClick={onNodeClick}
         nodesDraggable={false}
+        minZoom={0.4}
       >
         <Background gap={10} size={1} />
         <Controls position="bottom-right" />
