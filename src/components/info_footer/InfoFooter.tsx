ba946fdcff94960e5482075f473371a448463f93
hoangm960 <63330514+hoangm960@users.noreply.github.com>
2024-08-03 00:27:05 +0700

    add update footer

--- src/components/info_footer/InfoFooter.tsx
+++ src/components/info_footer/InfoFooter.tsx
@@ -2,6 +2,7 @@ import { useEffect, useState } from "react";
 import { InfoField } from "./InfoField"
 import { useStore } from "@nanostores/react";
 import { tableData } from "@/store/table_store";
+import useCourses from "@/hooks/getCourses";
 
 export const InfoFooter = () => {
     const [infoData, setInfoData] = useState({
@@ -16,8 +17,86 @@ export const InfoFooter = () => {
         el: 0,
     });
     const $tableData = useStore(tableData);
+    const GRADES = {
+        A: 4.0,
+        "A-": 3.7,
+        "B+": 3.3,
+        B: 3.0,
+        "B-": 2.7,
+        "C+": 2.3,
+        C: 2.0,
+        "C-": 1.7,
+        "D+": 1.3,
+        D: 1.0,
+        F: 0.0
+    };
+    const SPECIAL_GRADES = ["P", "NP", "I", "W"];
+    const [courseData, isCourseLoaded] = useCourses();
 
-    useEffect(() => console.log($tableData.filter((old) => old.term !== "")), [$tableData]);
+    useEffect(() => {
+        if (!isCourseLoaded) {
+            return;
+        }
+
+        const selectedCourses = $tableData.filter((old) => old.term !== "");
+        if (selectedCourses.length === 0) {
+            return;
+        }
+
+        let newInfoData = { ...infoData };
+
+        newInfoData.earned = selectedCourses
+            .map((course) => {
+                if ([...SPECIAL_GRADES, "F"].includes(course.grade)) {
+                    return 0;
+                }
+                return courseData[course.term][course.courseID]["credit"];
+            })
+            .reduce((total, credit) => total + credit, 0);
+
+        newInfoData.attempted = selectedCourses
+            .map((course) => courseData[course.term][course.courseID]["credit"])
+            .reduce((total, credit) => total + credit, 0);
+
+        newInfoData.core = selectedCourses.reduce(
+            (total, course) =>
+                total +
+                (courseData[course.term][course.courseID]["level"].toLowerCase() === "core" &&
+                    ![...SPECIAL_GRADES, "F"].includes(course.grade)),
+            0
+        );
+
+        let courseGPAMap = new Map(selectedCourses.map(course => [course.courseID, course]));
+        for (let course of selectedCourses) {
+            if (GRADES[course.grade] > GRADES[courseGPAMap.get(course.courseID)["grade"]])
+                courseGPAMap.set(course["code"], course);
+        }
+
+        let removeRepeated = [...courseGPAMap.values()];
+        newInfoData.gpa = parseFloat((
+            removeRepeated.reduce(
+                (total, course) => {
+                    if (SPECIAL_GRADES.includes(course.grade))
+                        return total;
+                    return total + GRADES[course.grade] * courseData[course.term][course.courseID]["credit"];
+                },
+                0
+            ) / newInfoData.attempted
+        ).toFixed(2));
+
+        let categories = ["e1", "e2", "e3", "e4", "el"];
+        for (let categoryKey of Object.keys(infoData)) {
+            if (categories.includes(categoryKey)) {
+                newInfoData[categoryKey] = selectedCourses.reduce(
+                    (total, course) =>
+                        total + courseData[course.term][course.courseID]["categories"].filter(category => category.toLowerCase() === categoryKey).length,
+                    0
+                );
+            }
+        }
+
+        setInfoData(newInfoData);
+    }, [$tableData]);
 
     return (
         <div
