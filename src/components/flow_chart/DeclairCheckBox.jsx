f1e3d3c96aa817aaa1d7a2a8a01b0c76dd6de9ba
hoangm960 <63330514+hoangm960@users.noreply.github.com>
2024-08-03 12:01:11 +0700

    setup new reactflow

--- src/components/flow_chart/DeclairCheckBox.jsx
+++
@@ -1,8 +0,0 @@
-export default function DeclairCheckBox({ updateFlow }) {
-    return (
-        <div className="flex items-center px-4 bg-action border-2 border-text rounded">
-            <input id="declair" type="checkbox" value="" className="w-5 h-5 text-blue-600 bg-gray-100 border-gray-300 rounded" onChange={updateFlow} defaultChecked/>
-            <label htmlFor="default-checkbox" className="w-full py-4 ms-2 text-lg font-semibold text-highlight">Declaired</label>
-        </div>
-    );
-}
