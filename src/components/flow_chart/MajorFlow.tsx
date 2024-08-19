191e5d794e022ae4e4e3990b1d3f34cdeb77e098
hoangm960 <63330514+hoangm960@users.noreply.github.com>
2024-08-19 23:37:27 +0700

    add style

--- src/components/flow_chart/MajorFlow.tsx
+++ src/components/flow_chart/MajorFlow.tsx
@@ -1,6 +1,6 @@
-import { ReactFlow } from '@xyflow/react';
-
+import { Background, Controls, ReactFlow, useEdgesState, useNodesState } from '@xyflow/react';
 import '@xyflow/react/dist/style.css';
+import MajorNode from './nodes/MajorNode';
 
 const initialNodes = [
     { id: '1', position: { x: 0, y: 0 }, data: { label: '1' } },
@@ -8,13 +8,28 @@ const initialNodes = [
 ];
 const initialEdges = [{ id: 'e1-2', source: '1', target: '2' }];
 
+const nodeTypes = { majorNode: MajorNode };
+
 export default function MajorFlow() {
+    const [nodes, setNodes, onNodesChange] = useNodesState(initialNodes);
+    const [edges, setEdges, onEdgesChange] = useEdgesState(initialEdges);
+
+
     return (
         <div className='w-full h-full'>
             <ReactFlow
-                nodes={initialNodes}
-                edges={initialEdges}
-            />
+                nodes={nodes}
+                edges={edges}
+                onNodesChange={onNodesChange}
+                onEdgesChange={onEdgesChange}
+                nodeTypes={nodeTypes}
+                fitView
+            >
+                <Background />
+                <Controls />
+
+            </ReactFlow>
+
         </div>
     );
 }
---
+++ src/components/flow_chart/nodes/MajorNode.tsx
@@ -0,0 +1,23 @@
+import { useCallback } from 'react';
+import { Handle, Position } from '@xyflow/react';
+
+const handleStyle = { left: 10 };
+
+export default function MajorNode({ data }) {
+
+  return (
+    <>
+      <Handle type="target" position={Position.Top} />
+      <div>
+        <label htmlFor="text">Text:</label>
+      </div>
+      <Handle type="source" position={Position.Bottom} id="a" />
+      <Handle
+        type="source"
+        position={Position.Bottom}
+        id="b"
+        style={handleStyle}
+      />
+    </>
+  );
+}
