b00f2e1126de2d4beabef05684772b7fbb478ed6
hoangm960 <63330514+hoangm960@users.noreply.github.com>
2024-09-17 05:20:52 +0700

    add fitview

--- src/components/flow_chart/Layout.tsx
+++ src/components/flow_chart/Layout.tsx
@@ -9,50 +9,10 @@ const DagreNodePositioning = () => {
     const edges = Array.from(edgeLookup).map(([, edge]) => edge);
     const { setNodes, setEdges } = useReactFlow();
 
-    const [nodesPositioned, setNodesPositioned] = useState(false);
 
     useEffect(() => {
         // node dimensions are not immediately detected, so we want to wait until they are
-        if (nodes[0]?.measured?.width) {
-            // create dagre graph
-            const dagreGraph = new dagre.graphlib.Graph();
-            // this prevents error
-            dagreGraph.setDefaultEdgeLabel(() => ({}));
 
-            // use dagre graph to layout nodes
-            const getLayoutedElements = (nodes, edges) => {
-                dagreGraph.setGraph({ rankdir: 'BT' });
-
-                edges.forEach((edge) => dagreGraph.setEdge(edge.source, edge.target));
-                nodes.forEach((node) => dagreGraph.setNode(node.id, node));
-
-                dagre.layout(dagreGraph);
-
-                return {
-                    nodes: nodes.map((node) => {
-                        const nodeWithPosition = dagreGraph.node(node.id);
-
-                        return {
-                            ...node, position: {
-                                x: nodeWithPosition.x - node.measured.width / 2,
-                                y: nodeWithPosition.y - node.measured.height / 2
-                            }
-                        };
-                    }),
-                    edges,
-                };
-            };
-
-            // if nodes exist and nodes are not positioned
-            if (nodes.length > 0 && !nodesPositioned) {
-                const layouted = getLayoutedElements(nodes, edges);
-
-                // update react flow state
-                setNodes(layouted.nodes);
-                setEdges(layouted.edges);
-                setNodesPositioned(true);
-            }
-        }
     }, [nodes, edges]);
 
     return null;
--- src/components/flow_chart/MajorFlow.tsx
+++ src/components/flow_chart/MajorFlow.tsx
@@ -1,16 +1,22 @@
-import { Background, Controls, ReactFlow, ReactFlowProvider, useEdgesState, useNodesState } from '@xyflow/react';
+import { Background, Controls, ReactFlow, ReactFlowProvider, useEdgesState, useNodesState, useReactFlow } from '@xyflow/react';
 import '@xyflow/react/dist/style.css';
 import MajorNode from './nodes/MajorNode';
-import { useEffect } from 'react';
+import { useEffect, useState } from 'react';
 import majorData from '@data/major.json';
 import DagreNodePositioning from './Layout';
+import dagre from "dagre";
+
 
 
 const nodeTypes = { majorNode: MajorNode };
 
-export default function MajorFlow() {
+function MajorFlow() {
     const [nodes, setNodes, onNodesChange] = useNodesState([]);
     const [edges, setEdges, onEdgesChange] = useEdgesState([]);
+    const { fitView } = useReactFlow();
+
+    const [nodesPositioned, setNodesPositioned] = useState(false);
+
 
     useEffect(() => {
         const majorNodes = majorData["MATH"]["flow-chart"]["major"]["nodes"].map((major, index) => {
@@ -36,22 +42,68 @@ export default function MajorFlow() {
         setEdges(majorEdges);
     }, []);
 
+    useEffect(() => {
+        if (nodes[0]?.measured?.width) {
+            const dagreGraph = new dagre.graphlib.Graph();
+            dagreGraph.setDefaultEdgeLabel(() => ({}));
+
+            const getLayoutedElements = (nodes, edges) => {
+                dagreGraph.setGraph({ rankdir: 'BT' });
+
+                edges.forEach((edge) => dagreGraph.setEdge(edge.source, edge.target));
+                nodes.forEach((node) => dagreGraph.setNode(node.id, node));
+
+                dagre.layout(dagreGraph);
+
+                return {
+                    nodes: nodes.map((node) => {
+                        const nodeWithPosition = dagreGraph.node(node.id);
+
+                        return {
+                            ...node, position: {
+                                x: nodeWithPosition.x - node.measured.width / 2,
+                                y: nodeWithPosition.y - node.measured.height / 2
+                            }
+                        };
+                    }),
+                    edges,
+                };
+            };
+
+            if (nodes.length > 0 && !nodesPositioned) {
+                const layouted = getLayoutedElements(nodes, edges);
+
+                setNodes(layouted.nodes);
+                setEdges(layouted.edges);
+                setNodesPositioned(true);
+            }
+        }
+
+        fitView();
+    }, [nodes, edges]);
+
+    return (
+        <ReactFlow
+            nodes={nodes}
+            edges={edges}
+            onNodesChange={onNodesChange}
+            onEdgesChange={onEdgesChange}
+            nodeTypes={nodeTypes}
+            fitView
+        >
+            <Background />
+            <Controls />
+
+        </ReactFlow>
+    );
+}
+
+export default function MajorFlowProvider() {
     return (
         <div className='w-full h-full'>
             <ReactFlowProvider>
                 <DagreNodePositioning />
-                <ReactFlow
-                    nodes={nodes}
-                    edges={edges}
-                    onNodesChange={onNodesChange}
-                    onEdgesChange={onEdgesChange}
-                    nodeTypes={nodeTypes}
-                    fitView
-                >
-                    <Background />
-                    <Controls />
-
-                </ReactFlow>
+                <MajorFlow />
             </ReactFlowProvider>
 
         </div>
